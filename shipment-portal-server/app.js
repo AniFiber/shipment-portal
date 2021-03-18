b2fe4049daca5500d1a037f83885c713ac6918df
HiDraqula <aniketaz190@gmail.com>
2021-03-18 09:08:49 +0530

    app.js - added comments

--- shipment-portal-server/app.js
+++ shipment-portal-server/app.js
@@ -1,21 +1,28 @@
+// Initializing dependencies (the one's which you will use)
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const app = express();
-const port = 5000;
+const app = express(); // Creating Express App
+const port = 5000; // port number on which server is listining on localhost
 
 const Api = require("./api");
 
-//Middlewares
+//// Middlewares
+// to parse req.body to json data
 app.use(express.json());
+// for enabling Cross-Origin Resource Sharing
 app.use(cors());
 
-//
-mongoose.connect("mongodb://localhost/test", {
+// Configuring Database Connection
+// Here "shipment-portal" is the name of project database (you can change it to any name you like)
+// Paste this address 👉 "mongodb://localhost/shipment-portal" in MongoDB Atlas to see Visual Representation
+mongoose.connect("mongodb://localhost/shipment-portal", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// Checking Database Connection
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
@@ -23,13 +30,14 @@ db.once("open", function () {
   console.log("MongoDB connected successfully");
 });
 
-//Routes
+//// Routes
 app.get("/", (req, res) => {
   res.send("Hello from the Server!! ✌");
 });
+// Api routes
 app.use("/api/shipment", Api);
 
-// Listener's
+//// Listener's
 app.listen(port, () => {
-  console.log(`Server is Listining On ${port}`);
+  console.log(`Server is Listining On https://localhost:${port}`);
 });
