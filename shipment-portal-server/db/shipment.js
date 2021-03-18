eab98debc3887879af73c53092de6e7bfc3ec05e
HiDraqula <aniketaz190@gmail.com>
2021-03-18 09:28:35 +0530

    added comments

--- shipment-portal-server/db/shipment.js
+++ shipment-portal-server/db/shipment.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// Here we are defining the things/schema which we to store in database
 const shipmentSchema = new Schema({
   blnumber: String,
   status: String,
 });
 
+// which is named "Shipment" for this collection of data
 const shipment = mongoose.model("Shipment", shipmentSchema);
 
+// Exporting it to use in other files (i.e. api.js/routes.js/controllers.js)
 module.exports = shipment;
